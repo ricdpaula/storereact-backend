7ea07c400184237332a29cc52ff87074283b7514
ricpaula <cadodepaulasilva159@gmail.com>
2025-03-13 16:49:49 -0300

    Update server js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,10 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import stripe from 'stripe'
-import {db} from './connect.js'
+import { db } from './connect.js'
 import cors from 'cors'
 
-dotenv.config({path: '../.env'})
+dotenv.config({ path: '../.env' })
 
 const stripeKey = stripe(process.env.STRIPE_KEY_TEST)
 
@@ -13,41 +13,41 @@ app.use(cors())
 app.use(express.static('public'))
 const port = process.env.PORT;
 
-app.get("/", (req, res)=>{
-    res.sendfile('index.html')
+app.get("/", (req, res) => {
+  res.sendfile('index.html')
 })
-app.get("/products", async (req, res)=>{
-    res.send(await db.collection('products').find({}).toArray())
+app.get("/products", async (req, res) => {
+  res.send(await db.collection('products').find({}).toArray())
 })
-app.get("/success", (req, res)=>{
-    res.send("Compra realizada com sucesso")
+app.get("/success", (req, res) => {
+  res.send("Compra realizada com sucesso")
 })
-app.get("/cancel", (req, res)=>{
-    res.send("Solicitação cancelada")
+app.get("/cancel", (req, res) => {
+  res.send("Solicitação cancelada")
 })
 
-app.post('/create-checkout-session/cart?', async (req, res) => {
-
-  const productsObj = []
-
-  const prices = req.query.priceid
-
-  prices.forEach((price) => {
-    const Product = new Object()
-    Product.price = price
-    Product.quantity = 1
-    productsObj.push(Product)
-  })
-    const session = await stripeKey.checkout.sessions.create({
-      line_items: productsObj,
-      mode: 'payment',
-      success_url: 'https://ricdpaula.github.io',
-      cancel_url: 'https://ricdpaula.github.io',
-    });
-  
-    res.redirect(303, session.url);
+app.post('/create-checkout-session', async (req, res) => {
+  const session = await stripeKey.checkout.sessions.create({
+    line_items: req.body,
+    mode: 'payment',
+    success_url: 'https://ricdpaula.github.io',
+    cancel_url: 'https://ricdpaula.github.io',
   });
 
-app.listen(port, ()=>{
-    console.log(`Conectado na porta ${port}`);
+  res.redirect(303, session.url);
+});
+
+app.listen(port, () => {
+  console.log(`Conectado na porta ${port}`);
 })
+
+// const productsObj = []
+
+  // const prices = req.query.priceid
+
+  // prices.forEach((price) => {
+  //   const Product = new Object()
+  //   Product.price = price
+  //   Product.quantity = 1
+  //   productsObj.push(Product)
+  // })
